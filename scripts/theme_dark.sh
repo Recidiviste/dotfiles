9df1b7a3a8e62f767add836652d347b52642278b
Recidiviste <luc.saccoccio@gmail.com>
2020-06-06 09:48:13 +0200

    Scripted Update

--- scripts/theme_dark.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wal -i /home/luc/Images/wallpapers/wallpaper968.png
-bash /home/luc/.config/polybar/scripts/styles.sh --Dark
--- scripts/theme_light.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wal -i /home/luc/Images/wallpapers/wallpaper1083.jpg
-bash /home/luc/.config/polybar/scripts/styles.sh --Light
